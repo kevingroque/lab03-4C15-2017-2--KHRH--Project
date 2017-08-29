114da019cf01f1e3e5c0ac8f37342a19a0626804
kevingroque <kevingroque@github.com>
2017-08-29 09:09:11 -0500

    sesion 03

---
+++ lab03AExampleGitHub/src/main/java/com/tecsup/lab03AExampleGitHub/StudentController.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03AExampleGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudentController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
